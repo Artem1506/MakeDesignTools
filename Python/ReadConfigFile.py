0684e389a57f00fefa8745190a5408677898510f
Zaitsev Pavel <paulworker3d@gmail.com>
2024-05-13 14:38:58 +0300

    Fix validation downloaded task and logit of work with Check state

--- Python/ReadConfigFile.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-config_file = "requests.config"
-parser = ConfigParser()
-parser.read(config_file, 'utf-16')
-catalog_name = parser.get('Variables','catalog_name')
-print(catalog_name)
--- Python/test2.py
+++
@@ -1,9 +0,0 @@
-mat_id_num = 5
-mat_parents_num = 1
-
-texts_ids = [3 , 1]
-             
-for i in range(mat_id_num):
-    print ('*')
-    if not (i + 1) in texts_ids:
-        print (i + 1)
